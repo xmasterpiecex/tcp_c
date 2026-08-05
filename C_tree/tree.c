24f20840c72326a3ec79cd360b11170f3fa685f3
xmasterpiecex <a211019971420125@gmail.com>
2026-08-05 19:34:45 +0300

    medium changes

--- C_tree/tree.c
+++ C_tree/tree.c
@@ -1,51 +1,96 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
+#include "stdbool.h"
 
 typedef struct Node
 {
-    int value;
+    char *value;
+    char *key;
     struct Node *left_node;
     struct Node *right_node;
 } Node;
 
-Node *create_node(int value)
+typedef struct List
+{
+    Node *root;
+} List;
+
+List *create_list()
+{
+    List *list = malloc(sizeof(List));
+    if (list == NULL)
+        return NULL;
+
+    list->root = NULL;
+    return list;
+}
+
+Node *create_node(char *value, char *key)
 {
     Node *node = malloc(sizeof(Node));
     node->value = value;
+    node->key = key;
     node->left_node = NULL;
     node->right_node = NULL;
     return node;
 }
-
-Node *insert_node(Node *root, int value)
+bool _insert_node(List *l, Node *node)
 {
-    Node *curr_node = root;
+    if (node == NULL || l == NULL)
+    {
+        return false;
+    }
+    l->root = node;
+    return true;
+}
 
+void delete_node(Node *n)
+{
+    free(n);
+}
+
+bool insert_node(List *root, char *value, char *key)
+{
     if (root == NULL)
-        return create_node(value);
+    {
+        return false;
+    }
+    
+    Node *curr_node = root->root;
+
+    if (root->root == NULL)
+    {
+        root->root = create_node(value, key);
+        return true;
+    }
+
     while (curr_node != NULL)
     {
-        Node **child = value < curr_node->value ? &curr_node->left_node : &curr_node->right_node;
+        int decider = strcmp(key, curr_node->key);
+
+        if (decider == 0)
+            return false;
+
+        Node **child = decider < 0 ? &curr_node->left_node : &curr_node->right_node;
 
         if ((*child) == NULL)
         {
-            (*child) = create_node(value);
-            break;
+            (*child) = create_node(value, key);
+            return true;
         }
-
         curr_node = *child;
     }
-    return root;
+    return _insert_node(root, create_node(value, key));
 }
 
-void delete_tree(Node *root)
+void delete_list(List *list)
 {
     Node *stack_buff[1000];
     memset(stack_buff, 0, sizeof(stack_buff));
     Node **stack_end = stack_buff;
-    
-    *stack_end = root;
+
+    *stack_end = list->root;
     stack_end++;
 
     while (stack_buff != stack_end)
@@ -64,16 +109,19 @@ void delete_tree(Node *root)
         }
         free(curr_node);
     }
+    free(list);
 }
 
 int main()
 {
-    Node *tree = NULL;
-    tree = insert_node(tree, 6);
-    tree = insert_node(tree, 4);
-    tree = insert_node(tree, 8);
+    List *tree = create_list();
+    insert_node(tree, "val1", "root node");
+    insert_node(tree, "ahha val", "some node");
+    insert_node(tree, "valhehe", "lala node");
 
-    delete_tree(tree);
+    // delete_tree(tree);
+
+    printf("root node key: %s", tree->root->key);
 
     return 1;
 }
